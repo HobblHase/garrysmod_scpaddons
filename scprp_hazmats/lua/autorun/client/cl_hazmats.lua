1fe4a705f662c787a48f3b04d0b2ecb416d6a67f
Hobbl <123955956+HobblHase@users.noreply.github.com>
2023-12-17 15:02:42 +0100

    Create cl_hazmats.lua

---
+++ scprp_hazmats/lua/autorun/client/cl_hazmats.lua
@@ -0,0 +1,28 @@
+-- written by HOPPL
+-- Simple SCP:RP Hazmat script that nerfs the immunity of hazmat with 20% more damage
+
+if CLIENT then
+    local function HazmatColorEffect()
+        local ply = LocalPlayer()
+
+        local clrs = {
+            ["$pp_colour_addr"] = 0.00,
+            ["$pp_colour_addg"] = 0.43,
+            ["$pp_colour_addb"] = 0,
+            ["$pp_colour_brightness"] = 0,
+            ["$pp_colour_contrast"] = 1,
+            ["$pp_colour_colour"] = 1,
+            ["$pp_colour_mulr"] = 0,
+            ["$pp_colour_mulg"] = 0,
+            ["$pp_colour_mulb"] = 0
+        }
+        if ply and ply:Alive() and HOPPL_HAZ_VISION and HOPPL_HAZ_MODELS[ply:GetModel()] then
+            // print("DEBUG: HAZMATVIEW") --only for me
+            DrawColorModify(clrs)
+        end
+    end
+
+    hook.Add("RenderScreenspaceEffects", "hoppl_haz_player_vision", HazmatColorEffect)
+
+    print("[HAZMATS] CL-LOADED")
+end
